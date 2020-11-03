c53f9e8500f850c35f5097efce14c1bf870064b1
Misha Ladonkin <ladonkin.mikhail@gmail.com>
2020-11-03 17:32:28 +0300

    change input event payload

--- src/broker/index.ts
+++ src/broker/index.ts
@@ -88,8 +88,7 @@ export default function createBroker(VirtualList: IVirtualList): IVirtualList {
             // Convert Draggable's change events to input events.
             change: (e: DraggableEvent<T>) => {
               if (draggableEvents.some(n => n in e)) {
-                this.$emit('input', draggablePolicy.updatedSources(
-                  e, this.vlsPolicy.draggingRealIndex), e);
+                this.$emit('input', e);
               }
             },
 
